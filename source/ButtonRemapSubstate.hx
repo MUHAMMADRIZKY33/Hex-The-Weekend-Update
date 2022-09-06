9bb2afe3f9ad550a7b8c3cc2c2bd3f12a758fcff
MUHAMMADRIZKY33 <105113724+MUHAMMADRIZKY33@users.noreply.github.com>
2022-09-06 11:46:14 +0700

    Update ButtonRemapSubstate.hx

--- source/ButtonRemapSubstate.hx
+++ source/ButtonRemapSubstate.hx
@@ -9,3 +9,41 @@ class ButtonRemapSubstate extends FlxSubState
 		super();
 	}
 }
+#if android
+addVirtualPad(LEFT_FULL, A_B);
+#end
+
+//if you want to remove it at some moment use
+#if android
+removeVirtualPad();
+#end
+
+//if you want it to have a camera
+#if android
+addPadCamera();
+#end
+
+//in states, these need to be added before super.create();
+//in substates, in fuction new at the last line add these
+
+//on Playstate.hx after all of the
+//obj.cameras = [...];
+//things, add
+#if android
+addAndroidControls();
+#end
+
+//if you want to remove it at some moment use
+#if android
+removeAndroidControls();
+#end
+
+//to make the controls visible the code is
+#if android
+androidControls.visible = true;
+#end
+
+//to make the controls invisible the code is
+#if android
+androidControls.visible = false;
+#end
